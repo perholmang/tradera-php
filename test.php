f219c040ec422231f3acbf46cb5f4400e37b242e
Per Holmäng <per.holmang@gmail.com>
2021-03-15 08:29:10 +0100

    Added GetSellerITems

--- test.php
+++ test.php
@@ -2,9 +2,8 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Holmang\Tradera\Search\SearchAdvancedRequest;
-use Holmang\Tradera\Search\SearchService;
 use Holmang\Tradera\PublicService\PublicService;
+use Holmang\Tradera\Search\SearchService;
 
 $service = new SearchService([
     'appId' => '1460',
@@ -27,12 +26,12 @@ $response = $service->searchAdvanced($request);
 var_dump($response);die;
 
 foreach ($response->Items as $item) {
-    echo $item->Id . ' - ' . $item->EndDate->format('Y-m-d H:i:s') . "\n";
+echo $item->Id . ' - ' . $item->EndDate->format('Y-m-d H:i:s') . "\n";
 }*/
 
 $public = new PublicService([
-'appId' => '1461',
-'appKey' => 'fe2b3cbf-fd5c-43b3-b341-1aa26f802d95' //'05f131ab-2389-467b-84d4-51ae2958824f'
+    'appId' => '1461',
+    'appKey' => 'fe2b3cbf-fd5c-43b3-b341-1aa26f802d95', //'05f131ab-2389-467b-84d4-51ae2958824f'
 ]);
 
 /*$public = new PublicService([
@@ -40,6 +39,6 @@ $public = new PublicService([
 'appKey' => '05f131ab-2389-467b-84d4-51ae2958824f'
 ]);*/
 
-$response = $public->GetItem(383719920);
+//$response = $public->GetItem(383719920);
 
-var_dump($response);
+$response = $public->GetSellerItems(156253, 2108);
